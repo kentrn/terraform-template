9efb0461959e88a0021ce7973e7d8c1151c7c43a
Kent Tran <kent@kents-mbp-2.home>
2019-07-26 22:57:57 +1000

    update to terraform v0.12 format

--- compute.tf
+++ compute.tf
@@ -1,103 +1,107 @@
 resource "aws_ami_from_instance" "terraform_ami_nodejs" {
-    name = "terraform_ami_nodejs"
-    source_instance_id = "${aws_instance.terraform_instance.id}"
+  name               = "terraform_ami_nodejs"
+  source_instance_id = aws_instance.terraform_instance.id
 }
 
 resource "aws_launch_configuration" "terraform_launch_config" {
-    name = "terraform_launch_config"
-    image_id = "${aws_ami_from_instance.terraform_ami_nodejs.id}"
-    instance_type = "t2.micro"
-    security_groups = ["${aws_security_group.terraform_sg_webserver.id}"]
-    associate_public_ip_address = true
-    lifecycle {
-        create_before_destroy = true
-    }
+  name                        = "terraform_launch_config"
+  image_id                    = aws_ami_from_instance.terraform_ami_nodejs.id
+  instance_type               = "t2.micro"
+  security_groups             = [aws_security_group.terraform_sg_webserver.id]
+  associate_public_ip_address = true
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_autoscaling_group" "terraform_autoscale_group" {
-    name = "terraform_autoscale_group"
-    max_size = "${var.aws["asg_max"]}"
-    min_size = "${var.aws["asg_min"]}"
-    health_check_grace_period = 300
-    health_check_type = "EC2"
-    desired_capacity = "${var.aws["asg_desired"]}"
-    vpc_zone_identifier = ["${aws_subnet.terraform_subnet_public_1.id}", "${aws_subnet.terraform_subnet_public_2.id}"]
-    launch_configuration = "${aws_launch_configuration.terraform_launch_config.name}"
-    termination_policies = ["NewestInstance"]
+  name                      = "terraform_autoscale_group"
+  max_size                  = var.aws["asg_max"]
+  min_size                  = var.aws["asg_min"]
+  health_check_grace_period = 300
+  health_check_type         = "EC2"
+  desired_capacity          = var.aws["asg_desired"]
+  vpc_zone_identifier       = [aws_subnet.terraform_subnet_public_1.id, aws_subnet.terraform_subnet_public_2.id]
+  launch_configuration      = aws_launch_configuration.terraform_launch_config.name
+  termination_policies      = ["NewestInstance"]
 }
 
 resource "aws_elb" "terraform_loadbalancer" {
-    name  = "terraform-loadbalancer"
-    subnets = ["${aws_subnet.terraform_subnet_public_1.id}", "${aws_subnet.terraform_subnet_public_2.id}"]
-    security_groups = ["${aws_security_group.terraform_sg_elb.id}"]
-    listener{
-        instance_port = 80
-        instance_protocol = "HTTP"
-        lb_port = 80
-        lb_protocol = "HTTP"
-    }
+  name            = "terraform-loadbalancer"
+  subnets         = [aws_subnet.terraform_subnet_public_1.id, aws_subnet.terraform_subnet_public_2.id]
+  security_groups = [aws_security_group.terraform_sg_elb.id]
+  listener {
+    instance_port     = 80
+    instance_protocol = "HTTP"
+    lb_port           = 80
+    lb_protocol       = "HTTP"
+  }
 
-    health_check {
-        healthy_threshold = 2
-        unhealthy_threshold = 2
-        timeout = 3
-        target = "TCP:80"
-        interval = 30
-    }
-    tags {
-        Name = "terraform_loadbalancer"
-    }
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "TCP:80"
+    interval            = 30
+  }
+  tags = {
+    Name = "terraform_loadbalancer"
+  }
 }
 
 resource "aws_autoscaling_attachment" "terraform_autoscaling_attachment" {
-    autoscaling_group_name = "${aws_autoscaling_group.terraform_autoscale_group.id}"
-    elb = "${aws_elb.terraform_loadbalancer.id}"
+  autoscaling_group_name = aws_autoscaling_group.terraform_autoscale_group.id
+  elb                    = aws_elb.terraform_loadbalancer.id
 }
+
 resource "tls_private_key" "instance_key" {
-    algorithm = "RSA"
-    rsa_bits = 4096
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
 resource "aws_key_pair" "terraform_generated_key" {
-    key_name = "terraform_generated_key"
-    public_key = "${tls_private_key.instance_key.public_key_openssh}"
+  key_name   = "terraform_generated_key"
+  public_key = tls_private_key.instance_key.public_key_openssh
 }
 
 resource "local_file" "terraform_generated_key" {
-    content = "${tls_private_key.instance_key.private_key_pem}"
-    filename = "terraform_generated_key.pem"
-    provisioner "local-exec" {
-        command = "chmod 400 terraform_generated_key.pem"
-    }
+  content  = tls_private_key.instance_key.private_key_pem
+  filename = "terraform_generated_key.pem"
+  provisioner "local-exec" {
+    command = "chmod 400 terraform_generated_key.pem"
+  }
 }
 
 resource "aws_instance" "terraform_instance" {
-    ami = "${var.aws["ami_id"]}"
-    instance_type = "t2.micro"
-    subnet_id = "${aws_subnet.terraform_subnet_public_1.id}"
-    associate_public_ip_address = true
-    vpc_security_group_ids = ["${aws_security_group.terraform_sg_webserver.id}"]
-    tags {
-        Name = "terraform_instance"
-        env = "${var.aws["env"]}"
-    } 
-    key_name = "${aws_key_pair.terraform_generated_key.key_name}"
-    provisioner "remote-exec" {
-        inline = ["sudo yum update -y",
-        "curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash - ",
-        "sudo yum install nodejs git -y",
-        "git clone https://github.com/kentrn/vue-express-template.git",
-        "cd vue-express-template/",
-        "npm install && sudo npm install forever -g",
-        "touch .env && echo \"PORT=80\" >> .env",
-        "sudo forever start app.js"]
+  ami                         = var.aws["ami_id"]
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.terraform_subnet_public_1.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.terraform_sg_webserver.id]
+  tags = {
+    Name = "terraform_instance"
+    env  = var.aws["env"]
+  }
+  key_name = aws_key_pair.terraform_generated_key.key_name
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash - ",
+      "sudo yum install nodejs git -y",
+      "git clone https://github.com/kentrn/vue-express-template.git",
+      "cd vue-express-template/",
+      "npm install && sudo npm install forever -g",
+      "touch .env && echo \"PORT=80\" >> .env",
+      "sudo forever start app.js",
+    ]
 
-        connection {
-            type = "ssh"
-            user = "ec2-user"
-            private_key = "${tls_private_key.instance_key.private_key_pem}"
-            agent = false
-
-        }
+    connection {
+      host        = coalesce(self.public_ip, self.private_ip)
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = tls_private_key.instance_key.private_key_pem
+      agent       = false
     }
+  }
 }
+
--- security.tf
+++ security.tf
@@ -1,154 +1,155 @@
 resource "aws_security_group" "terraform_sg_webserver" {
-    name = "terraform_sg_webserver"
-    description = "security group for terraform instance"
-    vpc_id = "${aws_vpc.terraform_vpc.id}"
+  name        = "terraform_sg_webserver"
+  description = "security group for terraform instance"
+  vpc_id      = aws_vpc.terraform_vpc.id
 
-    ingress {
-        protocol = "tcp"
-        from_port = 22
-        to_port = 22
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress {
-        protocol = "tcp"
-        from_port = 0
-        to_port = 65535
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress {
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        from_port = 443
-        to_port = 443
-    }
-    ingress {
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        from_port = 80
-        to_port = 80
-    }
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 65535
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 443
+    to_port     = 443
+  }
+  ingress {
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+  }
 }
 
 resource "aws_security_group" "terraform_sg_elb" {
-    name = "terraform_sg_elb"
-    vpc_id = "${aws_vpc.terraform_vpc.id}"
+  name   = "terraform_sg_elb"
+  vpc_id = aws_vpc.terraform_vpc.id
 
-    ingress = {
-        protocol = "tcp"
-        from_port = 80
-        to_port = 80
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress = {
-        protocol = "tcp"
-        from_port = 443
-        to_port = 443
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress = {
-        protocol = "-1"
-        from_port = 0
-        to_port = 0
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_default_network_acl" "terraform_default_nacl" {
-    default_network_acl_id = "${aws_vpc.terraform_vpc.default_network_acl_id}"
-    tags{
-        Name = "default_nacl"
-    }
+  default_network_acl_id = aws_vpc.terraform_vpc.default_network_acl_id
+  tags = {
+    Name = "default_nacl"
+  }
 }
 
 resource "aws_network_acl" "terraform_nacl_public" {
-  vpc_id = "${aws_vpc.terraform_vpc.id}"
-  subnet_ids = ["${aws_subnet.terraform_subnet_public_1.id}", "${aws_subnet.terraform_subnet_public_2.id}"]
+  vpc_id     = aws_vpc.terraform_vpc.id
+  subnet_ids = [aws_subnet.terraform_subnet_public_1.id, aws_subnet.terraform_subnet_public_2.id]
   egress {
-      protocol = "tcp"
-      rule_no = 100
-      action = "allow"
-      cidr_block = "0.0.0.0/0"
-      from_port = 0
-      to_port = 65535
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
   }
   ingress {
-      protocol = "tcp"
-      rule_no = 100
-      action = "allow"
-      cidr_block = "0.0.0.0/0"
-      from_port = 22
-      to_port = 22
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 22
+    to_port    = 22
   }
   ingress {
-      protocol = "tcp"
-      rule_no = 110
-      action = "allow"
-      cidr_block = "0.0.0.0/0"
-      from_port = 443
-      to_port = 443
+    protocol   = "tcp"
+    rule_no    = 110
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 443
+    to_port    = 443
   }
   ingress {
-      protocol = "tcp"
-      rule_no = 120
-      action = "allow"
-      cidr_block = "0.0.0.0/0"
-      from_port = 80
-      to_port = 80
+    protocol   = "tcp"
+    rule_no    = 120
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
   }
   ingress {
-      protocol = "tcp"
-      rule_no = 130
-      action = "allow"
-      cidr_block = "0.0.0.0/0"
-      from_port = 1024
-      to_port = 65535
+    protocol   = "tcp"
+    rule_no    = 130
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1024
+    to_port    = 65535
   }
-  tags{
-    Name = "terraform_nacl_public"      
+  tags = {
+    Name = "terraform_nacl_public"
   }
 }
 
 resource "aws_network_acl" "terraform_nacl_private" {
-  vpc_id = "${aws_vpc.terraform_vpc.id}"
-  subnet_ids = ["${aws_subnet.terraform_subnet_private_1.id}", "${aws_subnet.terraform_subnet_private_2.id}"]
+  vpc_id     = aws_vpc.terraform_vpc.id
+  subnet_ids = [aws_subnet.terraform_subnet_private_1.id, aws_subnet.terraform_subnet_private_2.id]
   egress {
-      protocol = "tcp"
-      rule_no = 100
-      action = "allow"
-      cidr_block = "0.0.0.0/0"
-      from_port = 1024
-      to_port = 65535
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1024
+    to_port    = 65535
   }
   ingress {
-      protocol = "tcp"
-      rule_no = 100
-      action = "allow"
-      cidr_block = "${var.aws["vpc_cidr"]}${var.aws["vpc_cidr_mask"]}"
-      from_port = 22
-      to_port = 22
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "${var.aws["vpc_cidr"]}${var.aws["vpc_cidr_mask"]}"
+    from_port  = 22
+    to_port    = 22
   }
   ingress {
-      protocol = "tcp"
-      rule_no = 110
-      action = "allow"
-      cidr_block = "${var.aws["vpc_cidr"]}${var.aws["vpc_cidr_mask"]}"
-      from_port = 443
-      to_port = 443
+    protocol   = "tcp"
+    rule_no    = 110
+    action     = "allow"
+    cidr_block = "${var.aws["vpc_cidr"]}${var.aws["vpc_cidr_mask"]}"
+    from_port  = 443
+    to_port    = 443
   }
   ingress {
-      protocol = "tcp"
-      rule_no = 120
-      action = "allow"
-      cidr_block = "${var.aws["vpc_cidr"]}${var.aws["vpc_cidr_mask"]}"
-      from_port = 80
-      to_port = 80
+    protocol   = "tcp"
+    rule_no    = 120
+    action     = "allow"
+    cidr_block = "${var.aws["vpc_cidr"]}${var.aws["vpc_cidr_mask"]}"
+    from_port  = 80
+    to_port    = 80
   }
-  tags{
-    Name = "terraform_nacl_private"      
+  tags = {
+    Name = "terraform_nacl_private"
   }
 }
 
 resource "aws_iam_instance_profile" "terraform_webserver_svc_profile" {
-    name = "terraform_webserver_service_profile"
-    role = "${var.aws["ws_service_role"]}"
+  name = "terraform_webserver_service_profile"
+  role = var.aws["ws_service_role"]
 }
+
